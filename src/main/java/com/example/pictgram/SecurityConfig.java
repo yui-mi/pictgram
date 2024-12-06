962ff52658640d2c8202a97d6e783f92efd42b77
yui <yuio.s.0731@gmail.com>
2024-12-06 19:58:05 +0900

    仮コミット1

--- src/main/java/com/example/pictgram/SecurityConfig.java
+++ src/main/java/com/example/pictgram/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.pictgram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -16,12 +24,17 @@ import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import com.example.pictgram.entity.SocialUser;
+import com.example.pictgram.entity.User;
+import com.example.pictgram.entity.User.Authority;
 import com.example.pictgram.filter.FormAuthenticationProvider;
 import com.example.pictgram.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	protected static Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
 	@Autowired
 	private UserRepository repository;
@@ -33,7 +46,6 @@ public class SecurityConfig {
 			throws Exception {
 		MvcRequestMatcher h2RequestMatcher = new MvcRequestMatcher(introspector, "/**");
 		h2RequestMatcher.setServletPath("/h2-console");
-
 		RequestMatcher publicMatchers = new OrRequestMatcher(
 				new AntPathRequestMatcher("/"),
 				new AntPathRequestMatcher("/favicon.ico"),
@@ -59,6 +71,13 @@ public class SecurityConfig {
 						.defaultSuccessUrl("/topics") // ログイン成功時の遷移先
 						.failureUrl("/login-failure") // ログイン失敗時の遷移先
 						.permitAll()) // 未ログインでもアクセス可能
+				.oauth2Login(oauth2 -> oauth2
+						.loginPage("/login") // ログイン画面
+						.defaultSuccessUrl("/topics") // ログイン成功時の遷移先
+						.failureUrl("/login-failure") // ログイン失敗時の遷移先
+						.permitAll() // 未ログインでもアクセス可能
+						.userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+								.oidcUserService(this.oidcUserService())))
 				.logout(logout -> logout
 						.logoutSuccessUrl("/logout-complete") // ログアウト成功時の遷移先
 						.invalidateHttpSession(true)
@@ -88,5 +107,27 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+		final OidcUserService delegate = new OidcUserService();
+		return (userRequest) -> {
+			OidcUser oidcUser = delegate.loadUser(userRequest);
+			OAuth2AccessToken accessToken = userRequest.getAccessToken();
+
+			log.debug("accessToken={}", accessToken);
+
+			oidcUser = new DefaultOidcUser(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUser.getUserInfo());
+			String email = oidcUser.getEmail();
+			User user = repository.findByUsername(email);
+			if (user == null) {
+				user = new User(email, oidcUser.getFullName(), "", Authority.ROLE_USER);
+				repository.saveAndFlush(user);
+			}
+			oidcUser = new SocialUser(oidcUser.getAuthorities(), oidcUser.getIdToken(), oidcUser.getUserInfo(),
+					user.getUserId());
+
+			return oidcUser;
+		};
 	}
 }
